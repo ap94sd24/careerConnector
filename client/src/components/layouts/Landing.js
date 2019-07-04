1e52fe4ee66b37cb29eea06923779eea43c4792d
ap94sd24 <adampan94@gmail.com>
2019-07-03 18:37:14 -0700

    Added stylesheet template and integrated landing pg into react dom, and auth routing

---
+++ client/src/components/layouts/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Career Connector</h1>
+          <p className='lead'>
+            Create a profile/portfolio, share posts and get help from
+            other like-minded professionals
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
