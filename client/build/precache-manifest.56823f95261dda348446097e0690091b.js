43a60a0d155744572c2f6a74568b52711b4115d0
Adam Pan <adampan94@gmail.com>
2020-05-23 16:37:03 -0700

    Deployment fix2

---
+++ client/build/precache-manifest.56823f95261dda348446097e0690091b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1b87265a59f0a14a1558d073611304",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e50c0d71bf9442efd7a6",
+    "url": "/static/css/main.9253a633.chunk.css"
+  },
+  {
+    "revision": "e5be7c07b72653ffdfb4",
+    "url": "/static/js/2.9f9d1072.chunk.js"
+  },
+  {
+    "revision": "30bb4e9efc588151f5fd9d30b8b21f9f",
+    "url": "/static/js/2.9f9d1072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50c0d71bf9442efd7a6",
+    "url": "/static/js/main.ffd7beb6.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "b2bf9fa04bffba3da4278130ea6843bf",
+    "url": "/static/media/showcase.b2bf9fa0.jpg"
+  },
+  {
+    "revision": "0d3e602a1fa8503912f83f49d04ab528",
+    "url": "/static/media/spinner.0d3e602a.gif"
+  }
+]);
